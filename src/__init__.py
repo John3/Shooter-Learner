0f3d8c71ab87633c5b42060c0396bea14ff2d842
Lukas Joergensen <lpjoergensen@gmail.com>
2017-03-24 13:32:09 +0100

    Support choosing feature-vectors

--- src/__init__.py
+++ src/__init__.py
@@ -9,7 +9,7 @@ from target_ddqrn import target_ddqrn
 
 train_length = 8  # todo do we need this?
 fv_size = 15  # Size of the FeatureVector (state)
-actions_size = 8  # Number of actions we can take
+actions_size = 9  # Number of actions we can take
 batch_size = 4  # Number of traces to use for each training step
 trace_length = 8  # How long each experience trace will be
 discount_factor = .99
